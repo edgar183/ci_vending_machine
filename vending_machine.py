fbee741f9dc66e8f85693ef7913c5f7032c931ba
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-02-08 13:35:51 +0000

    new test added and functinalaty of function updated to pass the test

--- vending_machine.py
+++ vending_machine.py
@@ -2,8 +2,20 @@
 from byotest import *
 
 def get_change(amount):
-    return []
+    """
+    Takes the payment amount and returns the change
+
+    `amount` the amount of money that we need to provide change for
+
+    Returns a list of coin values
+    """
+    if amount == 0:
+        return []
+    
+    return [1]
 
 #amount of change back is 0, no coins back
 test_are_equal(get_change(0),[])
+#amount requaered back is an single coin
+test_are_equal(get_change(1),[1])
 print("all tests pass!")
