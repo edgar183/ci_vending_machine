4d569355532a1f453a7aab86a3fce2e5935f1428
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-02-09 10:01:01 +0000

    new tes added to check when ther is insufficient coins to give change. using try catch code block

--- byotest.py
+++ byotest.py
@@ -47,3 +47,32 @@ def test_between(upper_limit, lower_limit, actual):
     Check to ensure that a number is between two other numbers. Raises
     AssertionError if the number is not between the other two numbers
     """
+def test_exception_was_raised(func, args, message):
+    """
+    Test that an error gets thrown inside of a given function. Raises
+    AssertionError if the error message was different from the expected
+    message
+    `func` is a reference to the function that is to be called
+    `args` is a tuple containing the arguments that are to be provided to
+        `func`
+    `message` is the string that is expected to be output by the error once
+        it's thrown
+    """
+    try:
+        # Call the function and unpack the `args` tuple by using `*`. This
+        # will unpack each of the items from the `args` tuple to pass 
+        # them into the function as arguments
+        func(*args)
+
+        # Execution will cease at this point if the error was successfully
+        # thrown, and will move onto the `except` block. If the
+        # function was successfully executed without throwing an error, we'll
+        # raise an AssertionError here to inform the developer that the
+        # function didn't throw an error as expected
+        assert False, "Exception was not raised"
+    except Exception as e:
+        # The message that was thrown will be stored in the exception
+        # instance as the first item in the list of `args`. This will allow us
+        # to check to see if the message that was thrown is the same as the
+        # message that the developer was expecting 
+        assert e.args[0] == message, "The message that was provided did not match the message thrown"
